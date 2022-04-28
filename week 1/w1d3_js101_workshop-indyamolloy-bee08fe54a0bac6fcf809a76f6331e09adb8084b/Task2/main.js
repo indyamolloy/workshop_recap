02055d01775ceda0d3bce6467e72137fff7017a8
Indya 2 <indyacmolloy@gmail.com>
2022-04-28 18:27:17 +1000

    task 3 completed

--- week 1/w1d3_js101_workshop-indyamolloy-bee08fe54a0bac6fcf809a76f6331e09adb8084b/Task2/main.js
+++ week 1/w1d3_js101_workshop-indyamolloy-bee08fe54a0bac6fcf809a76f6331e09adb8084b/Task2/main.js
@@ -1,1 +1,30 @@
+//Task 2
 console.log("Task 2");
+
+//Task 3.1
+let myFavNumber = 80;
+
+if (myFavNumber === 5) {
+  console.log("My favourite number is 5 too!");
+}
+
+//Task 3.2
+let myVariable = prompt(`What's your name?`);
+
+if (myVariable === "Ben") {
+  console.log(`That's my name too!`);
+} else {
+  console.log(myVariable + `!? That's a silly name...`);
+}
+
+//Task 3.3
+
+let weatherType = "sunny";
+
+if (weatherType === "raining") {
+  console.log("Take an umbrella!");
+} else if (weatherType === "sunny") {
+  console.log(`Don't forget suncream!`);
+} else {
+  console.log(`Must be a normal, gret non-descript day!`);
+}
