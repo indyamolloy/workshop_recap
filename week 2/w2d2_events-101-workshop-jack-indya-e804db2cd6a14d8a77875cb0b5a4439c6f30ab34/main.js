3ae3f62413a76cb00b4c441cbc2276f71cb4e535
Indya 2 <indyacmolloy@gmail.com>
2022-05-03 21:36:21 +1000

    tasks finished

--- week 2/w2d2_events-101-workshop-jack-indya-e804db2cd6a14d8a77875cb0b5a4439c6f30ab34/main.js
+++ week 2/w2d2_events-101-workshop-jack-indya-e804db2cd6a14d8a77875cb0b5a4439c6f30ab34/main.js
@@ -1,11 +1,51 @@
 const button = document.querySelector("#click-me");
 
 function handleClick(event) {
-  // If shift key pressed, change the inner text of the button to be NAILED IT
+  event.shiftKey ? (button.textContent = "NAILED IT!!") : null;
 }
 
 button.addEventListener("click", handleClick);
 
 document.querySelector("#title-changer").addEventListener("keyup", (e) => {
   console.log(e.target.value);
+});
+
+//Task 2
+const flowerButton = document.querySelector(".task-2");
+
+function titleChange() {
+  document.title = "💐🌷🌼";
+}
+
+flowerButton.addEventListener("click", titleChange);
+
+//Task 3
+const titleInput = document.querySelector("#title-changer");
+
+function updateH1(event) {
+  document.querySelector("h1").innerText = event.target.value;
+}
+
+titleInput.addEventListener("keyup", updateH1);
+
+//Task 4
+const image = document.querySelector("img");
+
+image.addEventListener("mouseenter", changeH1Color);
+image.addEventListener("mouseleave", removeH1Color);
+
+function changeH1Color() {
+  document.querySelector("h1").style.color = "hotpink";
+}
+
+function removeH1Color() {
+  document.querySelector("h1").style.color = "initial";
+}
+
+//Task 5
+const h1 = document.querySelector("h1");
+const checkbox = document.querySelector("#check-box");
+
+checkbox.addEventListener("change", () => {
+  h1.classList.toggle("funky");
 });
