30785a646a484633726184633326d26b16e0b0c6
Indya 2 <indyacmolloy@gmail.com>
2022-05-04 22:05:00 +1000

    kanye rest task finished

--- week02/w2d3_talking-to-servers-workshop-jack-indya-f563a2204f30ef2374312d69262f923f89de17b0/app.js
+++ week02/w2d3_talking-to-servers-workshop-jack-indya-f563a2204f30ef2374312d69262f923f89de17b0/app.js
@@ -1,1 +1,27 @@
 console.log("It's Kanye time...");
+
+async function getQuote() {
+  const response = await fetch("https://api.kanye.rest/");
+  const data = await response.json();
+  console.log(data.quote);
+  const h1 = document.querySelector("#kanye-quote");
+  h1.innerText = data.quote;
+  quoteHistory(data.quote);
+}
+
+getQuote();
+
+//add event to button click
+const button = document.querySelector("#new-quote-button");
+
+button.addEventListener("click", getQuote);
+
+//Save quotes to history
+function quoteHistory(quote) {
+  const listItem = document.createElement("li");
+  listItem.innerText = quote;
+
+  const list = document.querySelector("#kanye-quote-history");
+
+  list.appendChild(listItem);
+}
