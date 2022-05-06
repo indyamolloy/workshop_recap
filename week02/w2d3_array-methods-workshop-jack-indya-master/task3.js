1cb7e2952353d7f648018adef0343d55952dcd21
Indya 2 <indyacmolloy@gmail.com>
2022-05-06 21:09:21 +1000

    task 4 finished

--- week02/w2d3_array-methods-workshop-jack-indya-master/task3.js
+++ week02/w2d3_array-methods-workshop-jack-indya-master/task3.js
@@ -1,1 +1,10 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+//true
+const hasmultipleOf3 = someNumbers.some((number) => number % 3 === 0);
+//true
+const hasmultipleOf5 = someNumbers.some((number) => number % 5 === 0);
+//false
+const hasmultipleOf60 = someNumbers.some((number) => number % 60 === 0);
+//false
+const hasmultipleOf90 = someNumbers.some((number) => number % 90 === 0);
--- week02/w2d3_array-methods-workshop-jack-indya-master/task4.js
+++ week02/w2d3_array-methods-workshop-jack-indya-master/task4.js
@@ -1,3 +1,25 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
+//every method to check every number is multiple of 7
+//true
+const multiplesOfSeven = sevenTimesTable.every((number) => number % 7 === 0);
+
+//every method to check every number is multiple of 77
+//false
+const multiplesOfSeventySeven = seventySevenTimesTable.every(
+  (number) => number % 77 === 0
+);
+
+//which number isnt multiple of 77
+//461
+const numberNotMultipleOf77 = seventySevenTimesTable.find(
+  (number) => number % 77 !== 0
+);
+
+//use find index to find number position that isnt a multiple of 77
+//5
+const IndexOfNotMultiple = seventySevenTimesTable.findIndex(
+  (number) => number % 77 !== 0
+);
