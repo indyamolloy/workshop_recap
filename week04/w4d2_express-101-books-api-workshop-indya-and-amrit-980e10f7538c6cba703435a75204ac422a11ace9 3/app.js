3281b22274efab7d85be1b49e5b4031a64cc2581
Indya 2 <indyacmolloy@gmail.com>
2022-05-09 23:10:09 +1000

    task finished

--- week04/w4d2_express-101-books-api-workshop-indya-and-amrit-980e10f7538c6cba703435a75204ac422a11ace9 3/app.js
+++ week04/w4d2_express-101-books-api-workshop-indya-and-amrit-980e10f7538c6cba703435a75204ac422a11ace9 3/app.js
@@ -1,9 +1,28 @@
 import express from "express";
+import books from "./books-data.js";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the root path!" });
+});
+
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const result = books.find((book) => {
+    return book.id === id;
+  });
+
+  if (result !== undefined) {
+    res.json({ message: "book has been found", result: result });
+  } else {
+    res.json({ message: "book hasn't been found", result: "-" });
+  }
 });
 
 app.listen(port, () => {
