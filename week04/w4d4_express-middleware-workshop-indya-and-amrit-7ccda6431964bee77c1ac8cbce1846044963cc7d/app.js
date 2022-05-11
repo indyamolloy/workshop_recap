3a18be1812611ee4c7f39ac7aa384a336a466110
Indya 2 <indyacmolloy@gmail.com>
2022-05-11 20:24:29 +1000

    task 2 finished

--- week04/w4d4_express-middleware-workshop-indya-and-amrit-7ccda6431964bee77c1ac8cbce1846044963cc7d/app.js
+++ week04/w4d4_express-middleware-workshop-indya-and-amrit-7ccda6431964bee77c1ac8cbce1846044963cc7d/app.js
@@ -6,8 +6,18 @@ const app = express();
 
 app.use(express.json());
 
+//middleware to console log the request body and message
 app.use(function (req, res, next) {
   console.log("Request received!", req.body);
+  next();
+});
+
+//message to log time of request
+app.use(function (req, res, next) {
+  const date = new Date().toString();
+  req.body.timestamp = date;
+
+  console.log(req.body.timestamp);
   next();
 });
 
