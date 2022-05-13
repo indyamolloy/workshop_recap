4aca6e895c9fd1625361a597e6ff050496e98352
Indya 2 <indyacmolloy@gmail.com>
2022-05-13 18:09:04 +1000

    credentials added to env variables

---
+++ week05/w5d3_node-pg-codealong-indyapyi-2869560f4f877312e4e7aad5b3808e85bd564a0b/config.js
@@ -0,0 +1,5 @@
+export const dbuser = process.env.DB_USER;
+export const dbhost = process.env.DB_HOST;
+export const dbdatabase = process.env.DB_DATABASE;
+export const dbpassword = process.env.DB_PASSWORD;
+export const dbport = process.env.DB_PORT;
--- week05/w5d3_node-pg-codealong-indyapyi-2869560f4f877312e4e7aad5b3808e85bd564a0b/db/index.js
+++ week05/w5d3_node-pg-codealong-indyapyi-2869560f4f877312e4e7aad5b3808e85bd564a0b/db/index.js
@@ -1,12 +1,16 @@
 import pg from "pg";
+import * as config from "../config.js";
+
 const pool = new pg.Pool({
-  user: "fewrgngrccafue",
-  host: "ec2-176-34-211-0.eu-west-1.compute.amazonaws.com",
-  database: "d7rpbpbuiv7mrc",
-  password: "194fb1e4405e4e05d6e7b7929034d49f9991f92aa39c54a60f542c031a7430a0",
-  port: 5432,
+  user: config.dbuser,
+  host: config.dbhost,
+  database: config.dbdatabase,
+  password: config.dbpassword,
+  port: config.dbport,
   ssl: { rejectUnauthorized: false },
 });
+
+console.log(config.dbuser);
 
 export default function query(text, params) {
   return pool.query(text, params);
