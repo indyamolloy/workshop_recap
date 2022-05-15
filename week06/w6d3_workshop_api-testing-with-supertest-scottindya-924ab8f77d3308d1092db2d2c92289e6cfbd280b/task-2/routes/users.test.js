5bb5913c8ace42e1bb81b960328a6f65e6dbf636
Indya 2 <indyacmolloy@gmail.com>
2022-05-15 22:34:34 +1000

    testing /users route

--- week06/w6d3_workshop_api-testing-with-supertest-scottindya-924ab8f77d3308d1092db2d2c92289e6cfbd280b/task-2/routes/users.test.js
+++ week06/w6d3_workshop_api-testing-with-supertest-scottindya-924ab8f77d3308d1092db2d2c92289e6cfbd280b/task-2/routes/users.test.js
@@ -1,1 +1,24 @@
 // Write your tests for task 2 in this file. Plan out what you need to import/require.
+
+import request from "supertest";
+import app from "../app.js";
+
+test("Sends a get request and response with the structure {success: true, payload: users}.", async function () {
+  await request(app)
+    .get("/users")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .expect(function (res) {
+      const expected = { success: true, payload: expect.any(Array) };
+      const actual = res.body;
+
+      expect(actual).toStrictEqual(expected);
+
+      actual.payload.forEach((item) => {
+        expect(item).toStrictEqual({
+          id: expect.any(Number),
+          username: expect.any(String),
+        });
+      });
+    });
+});
