067f4f23def03f0c41c58f719ae9b674be25a615
Indya 2 <indyacmolloy@gmail.com>
2022-05-17 17:18:18 +1000

    Compliments added to button click

--- week07/w7d2_react-usestate-pt1-workshop-room-28-alex-indya-toni-308d865202bb5dafd19299f982822df0c2f37228/src/App.js
+++ week07/w7d2_react-usestate-pt1-workshop-room-28-alex-indya-toni-308d865202bb5dafd19299f982822df0c2f37228/src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 import { bootcampers } from "./bootcampers.js";
+import { compliments } from "./bootcampers.js";
 
 function App() {
   const [bootcamperIndex, setbootcamperIndex] = useState(0);
+  const [complimentIndex, setComplimentIndex] = useState(0);
 
   function handleClick() {
     const length = bootcampers.length;
     const randomNum = Math.floor(Math.random() * length);
 
     setbootcamperIndex(randomNum);
+
+    const complimentsLength = compliments.length;
+    const randomNumCompliments = Math.floor(Math.random() * complimentsLength);
+
+    setComplimentIndex(randomNumCompliments);
   }
+
   return (
     <div className="App">
       <h1>React useState</h1>
-      <p>{bootcampers[bootcamperIndex]}</p>
+      <p>
+        {bootcampers[bootcamperIndex]} {compliments[complimentIndex]}
+      </p>
       <button onClick={handleClick}>Click me for a random bootcamper!</button>
     </div>
   );
