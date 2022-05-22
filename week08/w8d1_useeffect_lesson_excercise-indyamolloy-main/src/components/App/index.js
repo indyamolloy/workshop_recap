3f8e4016cb6acdb4d293cc1f9a4d641e559422b2
Indya 2 <indyacmolloy@gmail.com>
2022-05-22 21:02:23 +1000

    Title changed tonumber of todos

--- week08/w8d1_useeffect_lesson_excercise-indyamolloy-main/src/components/App/index.js
+++ week08/w8d1_useeffect_lesson_excercise-indyamolloy-main/src/components/App/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import List from "../List";
 import Input from "../Input";
@@ -18,6 +18,10 @@ function App() {
     console.log("%cadd to do ", "color:lightgreen");
     setToDos([...toDos, text]);
   }
+
+  useEffect(() => {
+    document.title = `${toDos.length} things to do`;
+  }, [toDos]);
 
   return (
     <div className="App">
